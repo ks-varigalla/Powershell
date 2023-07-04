a6eb319165d69070c4da4f22cc1518f22492e09a
KS_Varigalla <52290834+ks-varigalla@users.noreply.github.com>
2023-07-04 09:34:49 +0300

    Create DiskSpaceReport.ps1

---
+++ DiskSpaceReport.ps1
@@ -0,0 +1,56 @@
+#********************************************************************************************************************************************************************
+#*************************************************************** DISK SPACE UTILIZATION REPORT **********************************************************************#
+#********************************************************************************************************************************************************************
+
+# Place the list of servers here
+#$servers = Get-Content -Path "C:\Temp\Diskcheck-servers.txt" 
+
+$date = Get-Date -Format dd-MM-yyyyT-hhmmss
+
+$servers = "MADWINTEL01","FISMGMT003"
+
+$outarr = @()
+
+$totarr = @()
+
+foreach($server in $servers){
+
+$result = Get-CimInstance -ComputerName $server -ClassName Win32_LogicalDisk | select @{Name="ServerName";Expression={$server}}, DeviceID, VolumeName, @{Name="Size(GB)";Expression={[Math]::Ceiling($_.size/1gb)}}, 
+
+@{Name="Freespace(GB)";Expression={[Math]::Ceiling($_.freespace/1gb)}}, @{Name="UsedSpace(GB)";Expression={[Math]::Ceiling(($_.size - $_.freespace)/1GB)}},
+
+@{Name="Used(%)";Expression={[Math]::Ceiling(($_.size - $_.freespace)/$_.size * 100)}}
+
+
+#Get the total disk used space
+
+$total = 0
+
+$atotal = 0
+
+foreach($item in $result){
+
+    $asize = $item.'Size(GB)'
+
+    $atotal+=$asize
+
+    $uspace = $item."UsedSpace(GB)"
+
+    $total+=$uspace
+
+}
+
+$obj = "" | Select @{Name="ServerName";Expression={$server}}, @{Name="Total Disk Size(GB)";e={$atotal}}, @{Name="Total Used Space(GB)";e={$total}},@{Name="Used(%)";e={[math]::Ceiling(($total/$atotal)*100)}},@{Name="Free Space(%)";e={[math]::Ceiling(($atotal-$total)/$atotal*100)}}
+
+$totarr+=$obj
+
+$outarr+=$result
+
+}
+
+$totarr | Export-Excel -Path "C:\Temp\Disk Space Reports\Disk Space Report-$date.xlsx" -WorksheetName "Total Size" -BoldTopRow -AutoSize -AutoFilter
+
+$outarr | Export-Excel -Path "C:\Temp\Disk Space Reports\Disk Space Report-$date.xlsx" -WorksheetName "Drive Size" -BoldTopRow -AutoSize -AutoFilter
+
+
+#*********************************************************************** END OF THE SCRIPT **************************************************************************
